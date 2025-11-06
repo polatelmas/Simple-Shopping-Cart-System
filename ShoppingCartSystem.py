58eec6dfb924f2c7fa7cee5a3c4a6aaf5714a86a
Polat Elmas <pltlms58@gmail.com>
2025-11-06 23:03:16 +0300

    Modified getting product quantity part with extended approach

--- ShoppingCartSystem.py
+++ ShoppingCartSystem.py
@@ -1,5 +1,10 @@
 cart = []
-
-print("Please enter how many products you want to add: ")
-productQuantity = input()
-
+while True:
+    try:
+        productQuantity = int(input("Please enter how many products you want to add: "))
+        if productQuantity >= 0:
+            break
+        else:
+            print("Please enter a number equal or bigger than zero.")
+    except ValueError:
+        print("You have to enter an integer.")
